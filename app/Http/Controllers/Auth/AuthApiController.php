b709df6d72a4f28fec79cb306b3ce2e3d164ffe7
Iorgama Porcely <iorgama.p@gmail.com>
2021-03-21 15:12:11 -0300

    Inclusão do middleware de autenticação

---
+++ app/Http/Controllers/Auth/AuthApiController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+
+class AuthApiController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        $jwt_token = null;
+
+        if (!$jwt_token = JWTAuth::attempt($credentials)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Email or Password',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        // get user authenticated
+        // $user = auth()->user();
+
+        return response()->json([
+            'success' => true,
+            // 'user' => $user,
+            'token' => $jwt_token,
+        ]);
+    }
+
+    public function getAuthenticatedUser()
+    {
+        // get user authenticated
+        if (!$user = JWTAuth::parseToken()->authenticatew()) {
+            return response()->json(['user_not_found'], 404);
+        }
+
+        return response()->json(['user' => $user]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,17 @@
 <?php
 
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Auth\AuthApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/produtos', [ProductController::class, 'index']);
-Route::post('/adicionar-produtos', [ProductController::class, 'store']);
-Route::put('/baixar-produtos/{id}', [ProductController::class, 'updateAmount']);
-Route::delete('/remover-produto/{id}', [ProductController::class, 'destroy']);
+
+Route::post('/auth', [AuthApiController::class, 'login']);
+Route::group([
+  'middleware' => 'auth:api',
+], function () {
+  //Route::get('/me', [AuthApiController::class, 'getAuthenticatedUser']);
+  Route::get('/produtos', [ProductController::class, 'index']);
+  Route::post('/adicionar-produtos', [ProductController::class, 'store']);
+  Route::put('/baixar-produtos/{id}', [ProductController::class, 'updateAmount']);
+  Route::delete('/remover-produto/{id}', [ProductController::class, 'destroy']);
+});
